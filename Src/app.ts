7ed5c1ebc54c6f7bcf97891d5f259604011e317a
Bensidney Ndungu <125551568+sidneyvonex@users.noreply.github.com>
2026-03-02 19:59:22 +0300

    Add new allowed origin for CORS

--- Src/app.ts
+++ Src/app.ts
@@ -22,7 +22,8 @@ const allowedOrigins = [
   process.env.FRONTEND_URL,
   'http://localhost:5173',
   'http://localhost:5174',
-  'http://localhost:3000'
+  'http://localhost:3000',
+  'https://bitsa-website-frontend.vercel.app'
 ];
 
 app.use(cors({
